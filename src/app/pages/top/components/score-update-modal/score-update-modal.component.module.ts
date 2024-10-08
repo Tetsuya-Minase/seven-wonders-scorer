adfd8b77b4a53d251140e7eb415d5e89de694796
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2024-10-08 23:43:35 +0900

    feat: add score input form.

--- src/app/pages/top/components/score-update-modal/score-update-modal.component.module.ts
+++ src/app/pages/top/components/score-update-modal/score-update-modal.component.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { ScoreUpdateModalComponent } from './score-update-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ScoreInputFormComponent } from './score-input-form/score-input-form.component';
 
 @NgModule({
   imports: [ReactiveFormsModule],
   exports: [ScoreUpdateModalComponent],
-  declarations: [ScoreUpdateModalComponent],
+  declarations: [ScoreUpdateModalComponent, ScoreInputFormComponent],
   providers: [],
 })
 export class ScoreUpdateModalModule {}
